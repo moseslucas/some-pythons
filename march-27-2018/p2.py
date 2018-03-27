4c28d49f0f29e6a76517ab7b277d9d06c9594b41
moseslucas <mo@moseslucas.com>
2018-03-27 11:45:57 +0800

    Omits apostrophe in p2.py of march-27-2018

--- march-27-2018/p2.py
+++ march-27-2018/p2.py
@@ -1,8 +1,16 @@
+def omitApostrophe(word):
+  deduct = 0
+  if (word[0] == '"'):
+    deduct+=1
+  if (word[-1] == '"'):
+    deduct+=1
+  return deduct
+
 def countIt(words):
   words = words.split(" ")
   words_count = []
   for word in words:
-      words_count.append(len(word))
+    words_count.append(len(word) - omitApostrophe(word))
   return words_count
   
 def start():
